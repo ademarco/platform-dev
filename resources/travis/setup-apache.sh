cd60f0195f0a4be3e5964486c5eef565c2ffbd74
Antonio De Marco <antonio@nuvole.org>
2016-11-12 10:52:07 +0100

    Refactor

--- resources/travis/setup-apache.sh
+++ resources/travis/setup-apache.sh
@@ -4,7 +4,6 @@ sudo cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default ~/.ph
 sudo a2enmod rewrite actions fastcgi alias
 echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 ~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm
-cat /etc/apache2/sites-available/*
 sudo cp -f resources/travis/vhost.conf /etc/apache2/sites-available/default
 sudo sed -e "s?PATH?$(pwd)?g" --in-place /etc/apache2/sites-available/default
 cat /etc/apache2/sites-available/default
